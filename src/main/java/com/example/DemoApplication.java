1aacb480e667cc69bc41fdb6bae5b60393d57f88
Madhura Bhave <mbhave@pivotal.io>
2017-02-21 14:39:23 -0800

    Initial commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
