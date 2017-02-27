5ce0021ebba3b73c679f6e52f4a0717c13399452
Madhura Bhave <mbhave@pivotal.io>
2017-02-27 12:07:33 -0800

    Add custom health indicator

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,25 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Bean
+	public HealthIndicator helloHealthIndicator() {
+		return new HealthIndicator() {
+			@Override
+			public Health health() {
+				return Health.up().withDetail("hello", "world").build();
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
